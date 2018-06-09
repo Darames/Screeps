314e140ffe84449bb790bc8fa7f24e57e527edaf
Darames <dracodragano@gmx.net>
2018-06-09 17:19:31 +0200

    upgrade pickup droped resources

--- main.js
+++ main.js
@@ -23,33 +23,24 @@ module.exports.loop = function () {
 
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
     var buildersLimit = 3;
-    // console.log('Harvesters: ' + harvesters.length);
-    
-    // for(var name in Game.rooms) {
-    //     console.log('Room "'+name+'" has '+Game.rooms[name].energyAvailable+' energy');
-    // }
 
     if(harvesters.length < harvestersLimit) {
         var newName = 'Harvester' + Game.time;
-        // console.log('Spawning new harvester: ' + newName);
         Game.spawns['Darames'].spawnCreep([WORK,WORK,MOVE], newName, 
             {memory: {role: 'harvester'}});
     }
     if(transporters.length < transportersLimit && harvesters.length == harvestersLimit) {
         var newName = 'Transporter' + Game.time;
-        // console.log('Spawning new Upgrader: ' + newName);
         Game.spawns['Darames'].spawnCreep([CARRY,CARRY,MOVE,MOVE], newName, 
             {memory: {role: 'transporter'}});
     }
     if(upgraders.length < upgradersLimit && harvesters.length == harvestersLimit) {
         var newName = 'Upgrader' + Game.time;
-        // console.log('Spawning new Upgrader: ' + newName);
         Game.spawns['Darames'].spawnCreep([WORK,CARRY,MOVE,MOVE], newName, 
             {memory: {role: 'upgrader'}});
     }
     if(builders.length < buildersLimit && harvesters.length == harvestersLimit ) {
         var newName = 'Builder' + Game.time;
-        // console.log('Spawning new Builder: ' + newName);
         Game.spawns['Darames'].spawnCreep([WORK,CARRY,MOVE,MOVE], newName, 
             {memory: {role: 'builder'}});
     }

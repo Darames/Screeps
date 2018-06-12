20307f26a3506da74e14448ce5c7b373ed237c04
Darames <dracodragano@gmx.net>
2018-06-12 21:27:37 +0200

    new transporter prio

--- role.transporter.js
+++ role.transporter.js
@@ -1,4 +1,5 @@
 var moveTo = require('tools.functions');
+var variables = require('variables');
 
 var roleTransporter = {
 
@@ -12,14 +13,34 @@ var roleTransporter = {
             // geting energy
             moveTo.sourceContainer(creep);
         } else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                return (structure.structureType == STRUCTURE_SPAWN && structure.energy < structure.energyCapacity)
-                    || (structure.structureType == STRUCTURE_EXTENSION && structure.energy < structure.energyCapacity) 
-                    || (structure.structureType == STRUCTURE_TOWER && structure.energy < structure.energyCapacity)
-                    || (structure.structureType == STRUCTURE_CONTAINER && structure.pos.inRangeTo(structure.room.controller, 3) && structure.store[RESOURCE_ENERGY] < structure.storeCapacity )
+
+            if(variables.spawn(creep).energy < variables.spawn(creep).energyCapacity){
+                targets = variables.spawn(creep);
+            }
+            for (i = 0; i < variables.extensions(creep).length; i++) {
+                if(variables.extensions(creep)[i].energy < variables.extensions(creep)[i].energyCapacity){
+                    targets = targets + variables.extensions(creep)[i];
                 }
-            });
+            } 
+            for (i = 0; i < variables.towers(creep).length; i++) {
+                if(variables.towers(creep)[i].energy < variables.towers(creep)[i].energyCapacity){
+                    targets = targets + variables.towers(creep)[i];
+                }
+            } 
+            for (i = 0; i < variables.container(creep).length; i++) {
+                if(variables.container(creep)[i].store[RESOURCE_ENERGY] < variables.container(creep)[i].storeCapacity && !variables.container(creep)[i].pos.inRangeTo(structure.room.controller, 3)) {
+                    targets = targets + variables.container(creep)[i];
+                }
+            } 
+
+            // var targets = creep.room.find(FIND_STRUCTURES, {
+            //     filter: (structure) => {
+            //     return (structure.structureType == STRUCTURE_SPAWN && structure.energy < structure.energyCapacity)
+            //         || (structure.structureType == STRUCTURE_EXTENSION && structure.energy < structure.energyCapacity) 
+            //         || (structure.structureType == STRUCTURE_TOWER && structure.energy < structure.energyCapacity)
+            //         || (structure.structureType == STRUCTURE_CONTAINER && structure.pos.inRangeTo(structure.room.controller, 3) && structure.store[RESOURCE_ENERGY] < structure.storeCapacity )
+            //     }
+            // });
             // targets = _.sortBy(targets, t => creep.pos.getRangeTo(t));
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) { creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}}); }
--- variables.js
+++ variables.js
@@ -14,7 +14,20 @@ var variables = {
         var container = creep.room.find(FIND_STRUCTURES, { filter: (structure) => { return structure.structureType == STRUCTURE_CONTAINER } });
         return container;
     },
+    spawn: function(creep) {
+        var spawn = creep.room.find(FIND_SPAWN);
+        return spawn;
+    },
+    extensions: function(creep) {
+        var extensions = creep.room.find(FIND_STRUCTURES, { filter: (structure) => { return structure.structureType == STRUCTURE_EXTENSION } });
+        return extensions;
+    },
+    towers: function(creep) {
+        var towers = creep.room.find(FIND_STRUCTURES, { filter: (structure) => { return  structure.structureType == STRUCTURE_TOWER } });
+        return towers;
+    }
 };
 
 
 module.exports = variables;
+

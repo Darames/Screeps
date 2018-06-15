04ceb9990443be31a0677297f32852300fe1e918
Darames <dracodragano@gmx.net>
2018-06-15 18:43:59 +0200

    room memory working

--- rooms.js
+++ rooms.js
@@ -6,25 +6,49 @@ var roomMemory = {
         
             var thisRoom = Game.rooms[roomName];
             
-            // for (var constructionSitesName in Game.constructionSites) {
-            // console.log(constructionSitesName);
-            // }
+            for (var constructionSitesName in Game.constructionSites) {
+            var constructionSites = true;
+            }
+            
             if(!thisRoom.memory.scanMode){
                 thisRoom.memory.scanMode = true;
             }
-            if(Game.constructionSites){
-                thisRoom.memory.scanMode = "preparing";
+            if(constructionSites){
+                thisRoom.memory.scanMode = "waiting";
             }
             
-            if(! Game.constructionSites && thisRoom.memory.scanMode === "preparing"){
+            if(!constructionSites && thisRoom.memory.scanMode === "waiting"){
                 thisRoom.memory.scanMode = true;
             }
+            
             if(thisRoom.memory.scanMode === true){
-                thisRoom.memory.sources = thisRoom.find(FIND_SOURCES).id;
-                thisRoom.memory.container = thisRoom.find(FIND_STRUCTURES, { filter: (structure) => { return structure.structureType == STRUCTURE_CONTAINER } }).id;
-                thisRoom.memory.spawn = thisRoom.find(FIND_STRUCTURES, { filter: (structure) => { return structure.structureType == STRUCTURE_SPAWN } }).id;
-                thisRoom.memory.extensions = thisRoom.find(FIND_STRUCTURES, { filter: (structure) => { return structure.structureType == STRUCTURE_EXTENSION } }).id;
-                thisRoom.memory.towers = thisRoom.find(FIND_STRUCTURES, { filter: (structure) => { return  structure.structureType == STRUCTURE_TOWER } }).id;
+                
+                thisRoom.memory.sources = new Array();
+                thisRoom.memory.container = new Array();
+                thisRoom.memory.spawn = new Array();
+                thisRoom.memory.extensions = new Array();
+                thisRoom.memory.towers = new Array();
+                
+                var sources = thisRoom.find(FIND_SOURCES);
+                for(i = 0; i < sources.length; i++){
+                    thisRoom.memory.sources.push(sources[i].id);
+                }
+                var container = thisRoom.find(FIND_STRUCTURES, { filter: (structure) => { return structure.structureType == STRUCTURE_CONTAINER } });
+               for(i = 0; i < container.length; i++){
+                    thisRoom.memory.container.push(container[i].id);
+                }
+                var spawn = thisRoom.find(FIND_STRUCTURES, { filter: (structure) => { return structure.structureType == STRUCTURE_SPAWN } });
+                for(i = 0; i < spawn.length; i++){
+                    thisRoom.memory.spawn.push(spawn[i].id);
+                }
+                var extensions = thisRoom.find(FIND_STRUCTURES, { filter: (structure) => { return structure.structureType == STRUCTURE_EXTENSION } });
+                for(i = 0; i < extensions.length; i++){
+                    thisRoom.memory.extensions.push(extensions[i].id);
+                }
+                var towers = thisRoom.find(FIND_STRUCTURES, { filter: (structure) => { return  structure.structureType == STRUCTURE_TOWER } });
+                for(i = 0; i < towers.length; i++){
+                    thisRoom.memory.towers.push(towers[i].id);
+                }
                 
                 thisRoom.memory.scanMode = false;
             }

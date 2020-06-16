a892e8b6f24ebbda5c37e7597697a8b26e72c227
Marc Hilten <marc.hilten@pixelgenau.com>
2020-06-16 07:56:17 +0200

    fix limit flags

--- flags.js
+++ flags.js
@@ -4,28 +4,31 @@ var flags = {
             let thisRoom = Game.rooms[Game.flags[flag].pos.roomName];
             let thisFlag = Game.flags[flag];
 
-            let command = thisFlag.name.substr(0, 4);
-            let operator = thisFlag.name.substr(5);
+            let command = '';
+            let operator = '';
 
             if (thisFlag.name.substr(0, 4) === 'uPos') {
                 thisRoom.memory.upgraderPos = thisFlag.pos;
-                thisFlag.remove;
+                thisFlag.remove();
             } else if (thisFlag.name.substr(0, 5) === 'claim') {
                 Memory.rooms.toClaim.push(thisRoom);
-                thisFlag.remove;
+                thisFlag.remove();
             } else if (thisFlag.name.substr(0, 5) === 'scout') {
                 Memory.rooms.toScout.push(thisRoom);
-                thisFlag.remove;
+                thisFlag.remove();
+            } else {
+                command = thisFlag.name.substr(0, 4);
+                operator = thisFlag.name.substr(4);
             }
 
-            if ((command === 'tran' || command === 'buil' || command === 'upgr') && operator) {
+            if ((command === 'tran' || command === 'buil' || command === 'upgr') && (operator === '+' || operator === '-')) {
                 let limit = thisRoom.memory.limits[command];
                 if (operator === '+') {
                     limit.value = limit.value + 1;
                 } else if (operator === '-') {
                     limit.value = limit.value - 1;
                 }
-                thisFlag.remove;
+                thisFlag.remove();
             }
         }
     }

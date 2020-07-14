25f0668f597db776720d195bbb957cc739de3f04
Marc Hilten <marc.hilten@pixelgenau.com>
2020-07-14 08:29:38 +0200

    transporter targets

--- creep.transporter.js
+++ creep.transporter.js
@@ -60,7 +60,7 @@ var transporter = {
     			}
     		}
 		
-			if (typeof thisRoom.storage  !== 'undefined') {
+			if (typeof thisRoom.storage  !== 'undefined' && thisRoom.storage  != '') {
 				let storage = thisRoom.storage;
 				if (storage.store[RESOURCE_ENERGY] < (storage.store.getCapacity() / 2)) {
 					storage.transportPriority = -1;

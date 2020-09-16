8e02bfede1a0315b993ca12ba3da748fb9551498
Marc Hilten <marc.hilten@pixelgenau.com>
2020-09-16 07:51:17 +0200

    fix blueprint

--- flags.js
+++ flags.js
@@ -68,8 +68,8 @@ let flags = {
             }
         }
     },
-    setBlueprint: function (print, thisFlag){
-        
+    setBlueprint: function (print, thisFlag, thisRoom){
+        let thisRoom = Game.rooms[Game.flags[flag].pos.roomName];
             if (typeof thisRoom.memory.blueprint.template !== 'undefined') {
                 thisRoom.memory.blueprint.templates.push({
                     name: print,
